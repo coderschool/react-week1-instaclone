149e27e5afaff3e1dbe56d4669f8a535f596ec00
Jieren Chen <jieren.chen@gmail.com>
2017-11-14 20:43:33 +0700

    demostrate props

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
     console.log("test");
     return (
       <div className="App">
+        <h2>{this.props.title}</h2>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
