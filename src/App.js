72a41bf7fa09dd17fa6ebbd24eb361645527d5a6
Jieren Chen <jieren.chen@gmail.com>
2017-11-14 20:46:34 +0700

    update state

--- src/App.js
+++ src/App.js
@@ -3,11 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: ""
+    }
+  }
+
+  changeDate() {
+    this.setState({
+      date: new Date().toString()
+    });
+  }
+
   render() {
-    console.log("test");
+    console.log(new Date().toString());
     return (
       <div className="App">
         <h2>{this.props.title}</h2>
+        <h2>{this.state.date}</h2>
+        <button onClick={this.changeDate.bind(this)}>Update</button>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
